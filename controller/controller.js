c198c6336a9097ea16b92e5a5d40a584d400865c
Dismes <yuhki.n@gmail.com>
2018-02-27 01:56:13 -0800

    wee?

--- controller/controller.js
+++ controller/controller.js
@@ -3,7 +3,12 @@ var express = require("express");
 var router = express.Router();
 
 
-
+router.get("/hi", function(req, res){
+    body = {
+        roll: 20,
+    }
+    res.render("index")
+})
 
 router.get("/", function(req, res){
     res.render("index")
